1133f7a136f4744fb776921ceb176b3905c4913b
gsak3l <gsakel5@gmail.com>
2020-06-05 10:08:43 +0300

    config on the app.py added

--- app.py
+++ app.py
@@ -15,7 +15,24 @@ from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = "./uploads"
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 
+#filter for the mime-types
+def allowed_files(filename): # this takes the last part of the file and checks if the format is allowed
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 app = Flask(__name__)
+
+
+@app.route("/api/posts", methods=["GET"]) # this handles get requests only
+def index():
+    if request.method == "GET":
+        return jsonify(data="posts main response")
+
+# doesn't work the way it was supposed to
+# @app.route("/api/addpost", methods=["POST"]) #this handles post requests only
+# def index2():
+#     if request.method =="POST":
+#         return jsonify(data="THIS IS SO COOL ACTUALLY")
 
 
 if __name__ == "__main__":
