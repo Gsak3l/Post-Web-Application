99e85376d4565af1f80bb0daf38be0d5bdf4a6e1
gsak3l <gsakel5@gmail.com>
2020-06-07 11:04:34 +0300

    image and other info is now being uploaded

--- angular-posts/src/app/flaskapi.service.ts
+++ angular-posts/src/app/flaskapi.service.ts
@@ -18,6 +18,15 @@ export class FlaskapiService {
   }
 
   public addPost(postObj: Post, image: any) {
-    console.log(postObj);
+    console.log(image);
+
+    const { title, content } = postObj;
+    const formData: FormData = new FormData();
+
+    formData.append('title', title);
+    formData.append('content', content);
+    formData.append('cover', image[0], image['filename']);
+
+    return this.httpClient.post<Post>(this.server + 'addpost', formData);
   }
 }
