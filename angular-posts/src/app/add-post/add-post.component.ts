dd521828f995f1556b201221a44da976b7697470
gsak3l <gsakel5@gmail.com>
2020-06-07 10:25:22 +0300

    postform, handleinput, and addpost functions added

--- angular-posts/src/app/add-post/add-post.component.ts
+++ angular-posts/src/app/add-post/add-post.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FlaskapiService } from '../flaskapi.service';
+import { Router } from '@angular/router';
+import { Post } from '../models/post';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.scss']
+  styleUrls: ['./add-post.component.scss'],
 })
 export class AddPostComponent implements OnInit {
+  constructor(
+    private flaskApiService: FlaskapiService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  public image: any = null; // list of files
+  public busy: boolean;
 
-  ngOnInit(): void {
+  public postForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    content: new FormControl('', Validators.required),
+    cover: new FormControl('', Validators.required),
+  });
+
+  public handleInput($event: Event) {
+    this.image = $event.target['files'];
+    console.log(this.image);
   }
 
+  public addPost(formData: Post){
+    this.busy = true;
+    this.flaskApiService.addPost(formData, this.image).subscribe(res => {
+      this.busy = false;
+      console.log(res);
+      this.router.navigate["/"];
+    })
+  }
+
+  ngOnInit(): void {}
 }
